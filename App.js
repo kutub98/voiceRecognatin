940c81a5c6da5860d2c5ad26fb4919c3eb362de6
Kutub Udd <socialnetwork98710@gmail.com>
2023-06-04 22:31:07 +0600

    adde

--- App.js
+++ App.js
@@ -96,13 +96,19 @@ export default function App() {
       </Picker>
 
       <TouchableOpacity onPress={startRecognition}>
-        <Text>Click to Start Voice Recognition</Text>
+        <Text><svg style={styles.microphone} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z" />
+        </svg>
+        </Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={stopRecognition}>
         <Text>Click to Stop Voice Recognition</Text>
       </TouchableOpacity>
       <Text style={styles.text}>Recognized Text: {recognizedText}</Text>
     </View>
+
+    // test branch 
+  
   );
 }
 
@@ -115,4 +121,9 @@ const styles = StyleSheet.create({
   text: {
     marginTop: 20,
   },
+  microphone:{
+    height: 30,
+    width: 30,
+    backgroundColor: "#f7f7f7"
+  }
 });
